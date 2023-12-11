693d1ad1a992221994ccf4dd50433c1fb58de9be
glomebom <gleb.khodosevich@gmail.com>
2023-12-11 11:15:55 +0300

    Реализация счетчика Реализовать лейбл счетчика, при загрузке отображется 0 Реализовать кнопку По нажатию кнопки увеличивать счетчик

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var counterLabel: UILabel!
+    @IBOutlet weak var counterButton: UIButton!
+    
+    var count: Int = 0
+    
+    @IBAction func counterButton(_ sender: Any) {
+        count += 1
+        counterLabel.text = "Значение счётчика: \(String(count))"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterLabel.text = "Значение счётчика: \(count)"
         // Do any additional setup after loading the view.
     }
 
 
 }
-
