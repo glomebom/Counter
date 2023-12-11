a5c77f81b07a352644dd9692b749e96ad48b6d4a
glomebom <gleb.khodosevich@gmail.com>
2023-12-11 22:06:11 +0300

    Добавить перечисление вариантов сообщений для истории изменений Добавить функцию изменения значения счетчика Добавить функцию перебора значений истории изменений  и их вывод в textView Добавить функцию прокрутки textView по мере добавления сообщений

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -10,20 +10,90 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var counterButton: UIButton!
+    @IBOutlet weak var counterMinus: UIButton!
+    @IBOutlet weak var counterPlus: UIButton!
+    @IBOutlet weak var counterReset: UIButton!
+    @IBOutlet weak var counterHistoryView: UITextView!
     
+    // Перечисление вариантов сообщений для истории изменений
+    enum counterHistoryViewText {
+        case plus
+        case minus
+        case belowZero
+        case reset
+    }
+    
+    // Переменная счетчика
     var count: Int = 0
     
-    @IBAction func counterButton(_ sender: Any) {
+    @IBAction func counterPlusTouch(_ sender: Any) {
+        
         count += 1
-        counterLabel.text = "Значение счётчика: \(String(count))"
+        
+        counterLabesTextUpdate(value: count)
+        
+        addMessageToHistoyView(message: .plus)
+        
+        scrollToBotom()
+    }
+    
+    @IBAction func counterMinusTouch(_ sender: Any) {
+        
+        if count > 0 {
+            
+            count -= 1
+            counterLabesTextUpdate(value: count)
+            addMessageToHistoyView(message: .minus)
+            
+        } else {
+            
+            addMessageToHistoyView(message: .belowZero)
+            
+        }
+
+        scrollToBotom()
+    }
+    
+    @IBAction func counterResetTouch(_ sender: Any) {
+        
+        count = 0
+        
+        counterLabesTextUpdate(value: count)
+        
+        addMessageToHistoyView(message: .reset)
+        
+        scrollToBotom()
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        counterLabel.text = "Значение счётчика: \(count)"
-        // Do any additional setup after loading the view.
+        counterLabesTextUpdate(value: count)
+        counterHistoryView.insertText("История изменений:" + "\n")
     }
-
-
+    
+    // Обновление значения счетчика
+    func counterLabesTextUpdate(value: Int) {
+        counterLabel.text = "Значение счётчика: \(String(value))"
+    }
+    
+    // Функция записи изменений в историю
+    func addMessageToHistoyView(message: counterHistoryViewText) {
+        switch message {
+            case .plus:
+                counterHistoryView.insertText("[\(Date.now.formatted())]: значение изменено на +1" + "\n")
+            case .minus:
+                counterHistoryView.insertText("[\(Date.now.formatted())]: значение изменено на -1" + "\n")
+            case .belowZero:
+                counterHistoryView.insertText("[\(Date.now.formatted())]: попытка уменьшить значение счётчика ниже 0" + "\n")
+            case .reset:
+                counterHistoryView.insertText("[\(Date.now.formatted())]: значение сброшено" + "\n")
+        }
+    }
+    
+    // Прокрутка к низу counterHistoryView после добавления строки
+    func scrollToBotom() {
+        let range = NSMakeRange((counterHistoryView.text as NSString).length - 1, 1);
+        counterHistoryView.scrollRangeToVisible(range);
+    }
+  
 }
