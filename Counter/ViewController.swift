2f21a64961d32153e07d40691695241e41a632da
glomebom <gleb.khodosevich@gmail.com>
2023-12-12 23:06:17 +0300

    UPD после review Задать модификаторы видимости для переменных и функций Привести наименование структуры в соответствие с camelCase Скорректировать орфографические ошибки наименований

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,15 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var counterLabel: UILabel!
-    @IBOutlet weak var counterMinus: UIButton!
-    @IBOutlet weak var counterPlus: UIButton!
-    @IBOutlet weak var counterReset: UIButton!
-    @IBOutlet weak var counterHistoryView: UITextView!
+    
+    @IBOutlet private weak var counterLabel: UILabel!
+    @IBOutlet private weak var counterMinus: UIButton!
+    @IBOutlet private weak var counterPlus: UIButton!
+    @IBOutlet private weak var counterReset: UIButton!
+    @IBOutlet private weak var counterHistoryView: UITextView!
     
     // Перечисление вариантов сообщений для истории изменений
-    enum counterHistoryViewText {
+    private enum CounterHistoryViewText {
         case plus
         case minus
         case belowZero
@@ -24,62 +24,62 @@ class ViewController: UIViewController {
     }
     
     // Переменная счетчика
-    var count: Int = 0
-    
-    @IBAction func counterPlusTouch(_ sender: Any) {
-        count += 1
-        counterLabesTextUpdate(value: count)
-        addMessageToHistoyView(message: .plus)
-        scrollToBotom()
-    }
-    
-    @IBAction func counterMinusTouch(_ sender: Any) {
-        if count > 0 {
-            count -= 1
-            counterLabesTextUpdate(value: count)
-            addMessageToHistoyView(message: .minus)
-        } else {
-            addMessageToHistoyView(message: .belowZero)
-        }
-        scrollToBotom()
-    }
-    
-    @IBAction func counterResetTouch(_ sender: Any) {
-        count = 0
-        counterLabesTextUpdate(value: count)
-        addMessageToHistoyView(message: .reset)
-        scrollToBotom()
-    }
+    private var count: Int = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        counterLabesTextUpdate(value: count)
+        counterLabelTextUpdate(value: count)
         counterHistoryView.insertText("История изменений:" + "\n")
     }
     
     // Обновление значения счетчика
-    func counterLabesTextUpdate(value: Int) {
+    private func counterLabelTextUpdate(value: Int) {
         counterLabel.text = "Значение счётчика: \(String(value))"
     }
     
     // Функция записи изменений в историю
-    func addMessageToHistoyView(message: counterHistoryViewText) {
+    private func addMessageToHistoryView(message: CounterHistoryViewText) {
         switch message {
-            case .plus:
-                counterHistoryView.insertText("[\(Date.now.formatted())]: значение изменено на +1" + "\n")
-            case .minus:
-                counterHistoryView.insertText("[\(Date.now.formatted())]: значение изменено на -1" + "\n")
-            case .belowZero:
-                counterHistoryView.insertText("[\(Date.now.formatted())]: попытка уменьшить значение счётчика ниже 0" + "\n")
-            case .reset:
-                counterHistoryView.insertText("[\(Date.now.formatted())]: значение сброшено" + "\n")
+        case .plus:
+            counterHistoryView.insertText("[\(Date.now.formatted())]: значение изменено на +1" + "\n")
+        case .minus:
+            counterHistoryView.insertText("[\(Date.now.formatted())]: значение изменено на -1" + "\n")
+        case .belowZero:
+            counterHistoryView.insertText("[\(Date.now.formatted())]: попытка уменьшить значение счётчика ниже 0" + "\n")
+        case .reset:
+            counterHistoryView.insertText("[\(Date.now.formatted())]: значение сброшено" + "\n")
         }
     }
     
     // Прокрутка к низу counterHistoryView после добавления строки
-    func scrollToBotom() {
+    private func scrollToBotom() {
         let range = NSMakeRange((counterHistoryView.text as NSString).length - 1, 1);
         counterHistoryView.scrollRangeToVisible(range);
     }
-  
+    
+    @IBAction private func counterPlusTouch(_ sender: Any) {
+        count += 1
+        counterLabelTextUpdate(value: count)
+        addMessageToHistoryView(message: .plus)
+        scrollToBotom()
+    }
+    
+    @IBAction private func counterMinusTouch(_ sender: Any) {
+        if count > 0 {
+            count -= 1
+            counterLabelTextUpdate(value: count)
+            addMessageToHistoryView(message: .minus)
+        } else {
+            addMessageToHistoryView(message: .belowZero)
+        }
+        scrollToBotom()
+    }
+    
+    @IBAction private func counterResetTouch(_ sender: Any) {
+        count = 0
+        counterLabelTextUpdate(value: count)
+        addMessageToHistoryView(message: .reset)
+        scrollToBotom()
+    }
+    
 }
